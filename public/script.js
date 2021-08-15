5e50241bd8e53f44344385e335e30572fb23a303
abbasam <tiggerwitht1378@gmail.com>
2021-08-15 09:28:48 +0430

    provide for depoly

--- public/script.js
+++ public/script.js
@@ -1,26 +1,26 @@
-let lessWord = 5;
-let contents = document.querySelectorAll(".content");
+var lessWord = 5;
+var contents = document.querySelectorAll(".content");
 
 contents.forEach(content => {
-    let changeToArray = content.textContent.split(' ');
+    var changeToArray = content.textContent.split(' ');
     console.log(Number(changeToArray.length))
     content.nextElementSibling.style.display = "inline";
     if (changeToArray.length < lessWord) {
         content.nextElementSibling.style.display = "none";
         content.nextElementSibling.nextElementSibling.style.display = "inline";
     } else if (changeToArray.length > lessWord && changeToArray.length <= 500) {
-        let displayText = content.textContent.substring(0, lessWord - 1);
-        let moreText = content.textContent.substring(lessWord - 1, Number(changeToArray.length));
+        var displayText = content.textContent.substring(0, lessWord - 1);
+        var moreText = content.textContent.substring(lessWord - 1, Number(changeToArray.length));
         content.innerHTML = `${displayText}<span class="dots">...</span><span class="more hide-more">${moreText}</span>`
     } else if (changeToArray.length > 500) {
-        let displayText = content.textContent.substring(0, lessWord - 1);
-        let moreText = content.textContent.substring(lessWord - 1, 500);
+        var displayText = content.textContent.substring(0, lessWord - 1);
+        var moreText = content.textContent.substring(lessWord - 1, 500);
         content.innerHTML = `${displayText}<span class="dots">...</span><span class="more hide-more">${moreText}</span><span class="dots1 hide-more">.....</span><span class="coment-for-showdetail hide-more">برای مطالعه کامل مقاله لطفا بروی ادامه مطلب کلیک کنید</span>`
     }
 })
 
 function readMore(btn) {
-    let post = btn.parentElement;
+    var post = btn.parentElement;
     post.querySelector(".dots").classList.add("hide-more");
     post.querySelector(".more").classList.remove("hide-more");
     post.querySelector(".dots1").classList.remove("hide-more");
